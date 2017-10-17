80ba00a8532b5f16bbf7bc9dbefb89c4dbe34561
Stephanie Strong <stephanie.s.strong@gmail.com>
2017-10-17 12:23:20 -0600

    Assignment 16 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
 require 'random_data'
 
+
+# post id: 51
+Post.find_or_create_by!(title: "New Title", body: "New Post")
+# post id: 52
+Post.find_or_create_by!(title: "Another new post title", body: "Another new body.")
+Post.find_or_create_by!(title: "Title three is Here", body: "Third post body content!")
+Post.find_or_create_by!(title: "Last one", body: "This is the last post added by find or create.")
+
+# comment id: 101 with post id:51
+Comment.find_or_create_by!(post: Post.find_or_create_by!(title: "New Title for a New Post", body: "New Body for a New Post"), body: "This is a comment")
+
+# comment id:102 with brand new post
+Comment.find_or_create_by!(post: Post.find_or_create_by!(title: "Post from a comment", body: "This post didn't exist before this comment was created"), body: "A self created comment")
+
 # Create Posts
 50.times do
   Post.create!(
