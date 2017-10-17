0833154abb538c8581555e94e771f5aac11107cc
Stephanie Strong <stephanie.s.strong@gmail.com>
2017-10-16 23:37:11 -0600

    Assignment 14 complete

---
+++ db/migrate/20171017052922_create_answers.rb
@@ -0,0 +1,10 @@
+class CreateAnswers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :answers do |t|
+      t.text :body
+      t.Question :reference
+
+      t.timestamps
+    end
+  end
+end
