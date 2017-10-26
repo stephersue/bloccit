e50e02dd362e20658612d835a9c9196adc4784f0
Stephanie Strong <stephanie.s.strong@gmail.com>
2017-10-26 12:40:10 -0600

    Checkpoint 18 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,43 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    # @posts.where(Post[i+1] % 5 == 0 || Post.first).update_all(title: "SPAM")
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
+
+  def create
+     @post = Post.new
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
+
+     if @post.save
+       flash[:notice] = "Post was saved."
+       redirect_to @post
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :new
+     end
+   end
+
 
   def edit
   end
+
+  # def censor
+  #   if Post[i+1]%5 == 0 || Post.first
+  #     Post.update(title: "SPAM")
+  #   end
+  # end
 end
