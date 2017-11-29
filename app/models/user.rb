2640997f7ed874421c0d9ba366b6663d87c66db6
Stephanie Strong <stephanie.s.strong@gmail.com>
2017-11-29 14:27:10 -0700

    assignment 23 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
 
+  before_save {
+    name_fix = []
+    self.name.split.each do |n|
+      name_fix << n.capitalize
+    end
+
+    self.name = name_fix.join(" ")
+  }
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
