9fb82ff44483325434309810d838c6311f2384f2
Stephanie Strong <stephanie.s.strong@gmail.com>
2017-12-06 16:12:56 -0700

    Assignment 27 complete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show]
+  before_action :authorize_user_to_edit, except: [:index, :show, :new, :create, :destroy]
+  before_action :authorize_user_to_create_or_delete, except: [:index, :show, :edit, :update]
 
   def index
      @topics = Topic.all
@@ -59,13 +60,20 @@ class TopicsController < ApplicationController
        render :show
      end
    end
-   
+
    private
    def topic_params
      params.require(:topic).permit(:name, :description, :public)
    end
 
-   def authorize_user
+   def authorize_user_to_edit
+        unless current_user.admin? || current_user.moderator?
+            flash[:alert] = "You must be an admin or moderator to do that."
+            redirect_to topics_path
+        end
+    end
+
+    def authorize_user_to_create_or_delete
      unless current_user.admin?
        flash[:alert] = "You must be an admin to do that."
        redirect_to topics_path
