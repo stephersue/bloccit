e3827b72ffcdef6a039b14d53880667343394c8c
Stephanie Strong <stephanie.s.strong@gmail.com>
2017-12-06 15:50:49 -0700

    Checkpoint 27 Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,11 +38,21 @@ posts = Post.all
   )
 end
 
-user = User.first
- user.update_attributes!(
-   email: 'youremail.com', # replace this with your personal email
+# Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
+
 
 puts "Seed finished"
 puts "#{User.count} users created"
