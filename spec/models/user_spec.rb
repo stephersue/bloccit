06c818b36d0f4ac58610bd72967e7befe7d8aec4
Stephanie Strong <stephanie.s.strong@gmail.com>
2017-12-07 16:30:21 -0700

    Updating User Rspec and Params for checkpoint 28

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe User, type: :model do
 
    it { is_expected.to have_many(:posts) }
    it { is_expected.to have_many(:comments) }
-   
    # Shoulda tests for name
    it { is_expected.to validate_presence_of(:name) }
    it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -25,60 +24,53 @@ RSpec.describe User, type: :model do
      it "should have name and email attributes" do
        expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
      end
+
+     it "should format the user's name" do
+       user.name = "bloc user"
+       user.save
+       expect(user.name).to eq "Bloc User"
+     end
+
+     it "responds to role" do
+       expect(user).to respond_to(:role)
+     end
+
+     it "responds to admin?" do
+       expect(user).to respond_to(:admin?)
+     end
+
+     it "responds to member?" do
+       expect(user).to respond_to(:member?)
+     end
    end
 
-   describe "invalid user" do
-     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
-
-     it "should be an invalid user due to blank name" do
-       expect(user_with_invalid_name).to_not be_valid
+   describe "roles" do
+     it "is member by default" do
+       expect(user.role).to eql("member")
      end
 
-     it "should be an invalid user due to blank email" do
-       expect(user_with_invalid_email).to_not be_valid
+     context "member user" do
+       it "returns true for #member?" do
+         expect(user.member?).to be_truthy
+       end
+
+       it "returns false for #admin?" do
+         expect(user.admin?).to be_falsey
+       end
+     end
+
+     context "admin user" do
+       before do
+         user.admin!
+       end
+
+       it "returns false for #member?" do
+         expect(user.member?).to be_falsey
+       end
+
+       it "returns true for #admin?" do
+         expect(user.admin?).to be_truthy
+       end
      end
    end
-
-   it "responds to role" do
-         expect(user).to respond_to(:role)
-       end
-
-       it "responds to admin?" do
-         expect(user).to respond_to(:admin?)
-       end
-
-       it "responds to member?" do
-         expect(user).to respond_to(:member?)
-       end
-     end
-
-     describe "roles" do
-       it "is member by default" do
-         expect(user.role).to eql("member")
-       end
-
-       context "member user" do
-         it "returns true for #member?" do
-           expect(user.member?).to be_truthy
-         end
-
-         it "returns false for #admin?" do
-           expect(user.admin?).to be_falsey
-         end
-       end
-
-       context "admin user" do
-         before do
-           user.admin!
-         end
-
-         it "returns false for #member?" do
-           expect(user.member?).to be_falsey
-         end
-
-         it "returns true for #admin?" do
-           expect(user.admin?).to be_truthy
-         end
-       end
-     end
+ end
